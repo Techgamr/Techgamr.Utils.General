0eac9e83ae2556bdd4e440fd2517344589a33d53
Techgamr <60068447+Techgamr@users.noreply.github.com>
2021-03-17 19:40:22 +0000

    Add Util.cs

---
+++ Techgamr.Utils.General/Util.cs
@@ -0,0 +1,7 @@
+﻿namespace Techgamr.Utils.General
+{
+    public static class Util
+    {
+        public static string ArrayToString(object[] array, string separator = ", ") => $"[{string.Join(separator, array)}]";
+    }
+}
