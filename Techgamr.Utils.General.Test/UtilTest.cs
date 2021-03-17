ff054e5bb08510c6e9b34f8407b5576dda4f65da
Techgamr <60068447+Techgamr@users.noreply.github.com>
2021-03-17 19:43:19 +0000

    Add tests

---
+++ Techgamr.Utils.General.Test/UtilTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Techgamr.Utils.General.Test
+{
+    [TestFixture]
+    public class UtilTest
+    {
+        [TestCase(new object[]{0}, "[0]", "ThisShouldNeverBeInTheResult")]
+        [TestCase(new object[]{0, 5, 2}, "[0, 5, 2]", ", ")]
+        [TestCase(new object[]{1200, 15423, 20000}, "[1200,15423,20000]", ",")]
+        [TestCase(new object[]{2147483647, 1, -2147483647}, "[2147483647 1 -2147483647]", " ")]
+        public void ArrayToStringTest(object[] actual, string expected, string joiner) => Assert.IsTrue(Util.ArrayToString(actual, joiner) == expected);
+    }
+}
