640b92236b4ea7a08345dd52b099a9edbdb56033
Techgamr <60068447+Techgamr@users.noreply.github.com>
2021-04-07 10:53:24 +0100

    Add generic ArrayToString

--- Techgamr.Utils.General.Test/UtilTest.cs
+++ Techgamr.Utils.General.Test/UtilTest.cs
@@ -10,5 +10,12 @@ namespace Techgamr.Utils.General.Test
         [TestCase(new object[]{1200, 15423, 20000}, "[1200,15423,20000]", ",")]
         [TestCase(new object[]{2147483647, 1, -2147483647}, "[2147483647 1 -2147483647]", " ")]
         public void ArrayToStringTest(object[] actual, string expected, string joiner) => Assert.IsTrue(Util.ArrayToString(actual, joiner) == expected);
+
+        [Test]
+        public void ArrayToStringGeneric()
+        {
+            var res = Util.ArrayToString(new[] {"thing1", "thing2"});
+            Assert.IsTrue(res == "[thing1, thing2]");
+        }
     }
 }
--- Techgamr.Utils.General/Util.cs
+++ Techgamr.Utils.General/Util.cs
@@ -3,5 +3,8 @@
     public static class Util
     {
         public static string ArrayToString(object[] array, string separator = ", ") => $"[{string.Join(separator, array)}]";
+
+        // ReSharper disable once ParameterTypeCanBeEnumerable.Global
+        public static string ArrayToString<TArray>(TArray[] array, string separator = ", ") => $"[{string.Join(separator, array)}]";
     }
 }
